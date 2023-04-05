bd2fe4326468c442ba1b76f3754d5a92c0c136e2
Ba6aka <bam6ykba6aka@gmail.com>
2023-04-05 21:55:51 +0300

    Create file delete-messages.js
    functions for delete message when user push button 'delete' in message.html

---
+++ public/JS/delete-messages.js
@@ -0,0 +1,24 @@
+function deleteMessages() {
+    const valueString = localStorage.getItem('myKey');
+    const values = JSON.parse(valueString);
+
+    if (values.forEach) {
+        values.forEach(element => {
+            let msg = (document.getElementById(element).parentElement)
+            msg.remove()
+        });
+    }
+}
+
+const messageContainer = document.querySelector('.messages');
+messageContainer.addEventListener('click', (e) => {
+    if (e?.target.nodeName == 'BUTTON') {
+        const values = JSON.parse(localStorage.getItem('myKey')) || [];
+
+        values.push(e.target.id);
+
+        localStorage.setItem('myKey', JSON.stringify(values));
+
+        location.reload();
+    }
+});
