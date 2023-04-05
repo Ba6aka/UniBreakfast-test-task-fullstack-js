05967083dd1610b1dcc332aa0c0ecfdebf2459fd
Ba6aka <bam6ykba6aka@gmail.com>
2023-04-05 21:39:57 +0300

    Add function renderMessages
    Function for get HTML for messages.html from dataBase

--- server.js
+++ server.js
@@ -6,25 +6,32 @@ createServer(async function (req, res) {
     if (req.method == 'GET') {
         serveFile(req.url, res)
     } else {
-        const message = await getBody(req);
-
-        saveFile(message, res, 'messages');
+        const message = await getBody(req)
+        if (req.url == '/postCalculation') {
+            saveFile(message, res, 'numbers')
+        } else {
+            saveFile(message, res, 'messages')
+        }
 
     }
+}).listen(1337)
 
-}
-).listen(1337)
-
+//! Serve and save files
 function serveFile(path, res) {
     let file;
     try {
         switch (path) {
             case '/message.html':
                 file = readFileSync(__dirname + '/public/message.html', 'utf-8')
+                const html = renderMessages()
+                file = file.replace('{smth}', html)
                 break;
             case '/':
                 path += 'index.html'
                 file = readFileSync(__dirname + '/public' + path)
+                break
+            case '/getCalculation':
+                file = calculation()
                 break
             case '/db.json':
                 file = readFileSync(__dirname + path)
@@ -34,10 +41,13 @@ function serveFile(path, res) {
         }
         res.end(file)
     }
+
     catch {
-        file = readFileSync(__dirname + '/404.html');
+        file = readFileSync(__dirname + '/public/404.html');
         res.end(file);
     }
+
+
 }
 
 
@@ -57,4 +67,26 @@ async function getBody(req) {
     for await (const chunk of req) body += chunk
 
     return body
+}
+
+function renderMessages() {
+    const json = readFileSync(__dirname + '/db.json', 'utf-8');
+    const arr = JSON.parse(json)['messages'];
+
+    let html = '';
+
+    for (let i of arr) {
+        html += `
+        <li>
+      <p>${i.message}</p>
+      <div class="row">
+          <span class="author">${i.author}</span>
+          <span class="date">${i.dataTime}</span>
+      </div>
+      <button id='${i.id}'>delete</button>
+      </li>
+    `
+    };
+
+    return html
 }
