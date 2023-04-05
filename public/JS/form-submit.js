7521bb3982602d9aecf7da3a9912d46742adce12
Ba6aka <bam6ykba6aka@gmail.com>
2023-04-05 21:32:33 +0300

    Add functions getTIme and getId

--- public/JS/form-submit.js
+++ public/JS/form-submit.js
@@ -3,6 +3,9 @@ const form = document.querySelector('form')
 form.addEventListener('submit', () => {
     const formData = new FormData(form);
 
+    formData.append('dataTime', getTime())
+    formData.append('id', getId())
+
     postData('/db.json', Object.fromEntries(formData))
         .then((response) => {
             console.log(response)
@@ -15,3 +18,24 @@ form.addEventListener('submit', () => {
         )
     location.reload()
 })
+
+
+function getTime() {
+    const today = new Date();
+    const now = today.toLocaleString();
+
+    return now
+}
+
+function getId() {
+    if (!localStorage.getItem('id')) {
+        let id = 1
+    }
+    let id = localStorage.getItem('id');
+
+    id++
+
+    localStorage.setItem('id', id)
+
+    return id
+}
